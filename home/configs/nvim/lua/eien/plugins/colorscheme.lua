fd3551cfce28293186aa0ed0c2323d02a12493d8
Eien <oleksiak-sebastian@proton.me>
2025-09-28 10:35:06 +0200

    changed colorscheme in nvim to catppuccin, deleted border, changed background, added shadows, opcaity rules

--- home/configs/nvim/lua/eien/plugins/colorscheme.lua
+++ home/configs/nvim/lua/eien/plugins/colorscheme.lua
@@ -1,7 +1,12 @@
 return {
-	"slugbyte/lackluster.nvim",
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme lackluster-hack")
+		require("catppuccin").setup({
+			flavour = "latte",
+		})
+
+		vim.cmd("colorscheme catppuccin")
 	end,
 }
