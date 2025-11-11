b1acd25fe5bcb67dd021249b13348fd6128bc135
Eien <oleksiak-sebastian@proton.me>
2025-11-11 10:55:21 +0100

    if fullscreen is closed then it shows the polybar again now

--- home/configs/polybar/hide-on-fullscreen.sh
+++ home/configs/polybar/hide-on-fullscreen.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 i3-msg -t subscribe -m '[ "window" ]' | while read -r line; do
+  change=$(echo "$line" | jq -r '.change')
   container=$(echo "$line" | jq -r '.container')
   fullscreen=$(echo "$container" | jq -r '.fullscreen_mode')
   output=$(echo "$container" | jq -r '.output')
@@ -8,12 +9,18 @@ i3-msg -t subscribe -m '[ "window" ]' | while read -r line; do
   if [ "$output" = 'DVI-D-0' ]; then
     if [ "$fullscreen" = '1' ]; then
       polybar-msg -p "$(pgrep -f 'polybar main1')" cmd hide
+      if [ "$change" = 'close' ]; then
+        polybar-msg -p "$(pgrep -f 'polybar main1')" cmd show
+      fi
     else
       polybar-msg -p "$(pgrep -f 'polybar main1')" cmd show
     fi
   else
     if [ "$fullscreen" = '1' ]; then
       polybar-msg -p "$(pgrep -f 'polybar main2')" cmd hide
+      if [ "$change" = 'close' ]; then
+        polybar-msg -p "$(pgrep -f 'polybar main2')" cmd show
+      fi
     else
       polybar-msg -p "$(pgrep -f 'polybar main2')" cmd show
     fi
