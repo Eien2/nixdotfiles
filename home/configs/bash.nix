7140016d1965f13f75ef1f34a1b42d62c59d1380
Eien <oleksiak-sebastian@proton.me>
2025-11-23 15:47:07 +0100

    changed displays to the correct one

--- home/configs/bash.nix
+++ home/configs/bash.nix
@@ -21,7 +21,7 @@
         PS1='\n\[\e[38;2;180;212;207m\]\u\[\e[38;2;205;205;205;1m\][\[\e[22m\]\[\e[38;2;110;148;178m\]\w\[\e[38;2;205;205;205;1m\]]\[\e[0m\]'
         export DOT=/home/eien/dotfiles
         export DISPLAY=:0.0
-        export MONITOR=DVI-D-1
+        export MONITOR=DVI-D-0
         export EDITOR=neovim
         export TERM=xterm-kitty
         export PATH="$HOME/bin:$PATH"
