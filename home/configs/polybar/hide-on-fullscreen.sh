cb86ad00a61ea36f70eeb926c4f6606081db5ebd
Eien <oleksiak-sebastian@proton.me>
2025-12-29 02:24:36 +0100

    transformed polybar so now it doesn't interact with discord notification

--- home/configs/polybar/hide-on-fullscreen.sh
+++ home/configs/polybar/hide-on-fullscreen.sh
@@ -9,24 +9,18 @@ i3-msg -t subscribe -m '[ "window" ]' | while read -r line; do
   if [ "$output" = 'DVI-D-0' ]; then
     if [ "$fullscreen" = '1' ]; then
       polybar-msg -p "$(pgrep -f 'polybar main1')" cmd quit
-      if [ "$change" = 'close' ]; then
+    elif [ "$change" = 'close' ] || [ "$change" = 'focus' ]  || [ "$change" = 'fullscreen_mode' ]; then
         killall polybar
         polybar main1 & disown
-      fi
-    else
-      killall polybar
-      polybar main1 & disown
     fi
   else
     if [ "$fullscreen" = '1' ]; then
       polybar-msg -p "$(pgrep -f 'polybar main2')" cmd quit
-      if [ "$change" = 'close' ]; then
-        killall polybar
-        polybar main2 & disown
-      fi
-    else
+    elif [ "$change" = 'close' ] || [ "$change" = 'focus' ] || [ "$change" = 'fullscreen_mode' ]; then
       killall polybar
       polybar main2 & disown
     fi
   fi
 done
+
+  
